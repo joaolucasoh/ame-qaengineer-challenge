a3de7bcdb3c11cdd80e766021f90f3c5cd52620b
Joao Lucas <luucaas.o.p@gmail.com>
2020-05-26 16:25:33 -0300

    Ajustando o report

--- ame-frontend/features/support/hooks.rb
+++ ame-frontend/features/support/hooks.rb
@@ -14,36 +14,41 @@ Before('@authpage?') do
   find('#create-account_form') && find('#login_form')
 end
 
+After do |scenario|
+  screenshot = page.save_screenshot("log/screenshots/#{scenario.__id__}.png")
+  embed(screenshot, 'image/png', 'Screenshot')
+end
+
 After do
   Capybara.execute_script 'localStorage.clear()'
 end
 
-After do
-  temp_shot = page.save_screenshot('log/image.png')
-  screenshot = Base64.encode64(File.open(temp_shot).read)
-  embed(screenshot, 'image/png', 'Screenshot')
-end
+# After do
+#   temp_shot = page.save_screenshot('log/image.png')
+#   screenshot = Base64.encode64(File.open(temp_shot).read)
+#   embed(screenshot, 'image/png', 'Screenshot')
+# end
 
-at_exit do
-  @infos = {
-    'Cliente' => 'AME Digital'.upcase,
-    'Data do Teste' => Time.now.to_s
-  }
+# at_exit do
+#   @infos = {
+#     'Cliente' => 'AME Digital'.upcase,
+#     'Data do Teste' => Time.now.to_s
+#   }
 
-  ReportBuilder.configure do |config|
-    config.input_path = 'log/report.json'
-    config.report_path = 'log/report'
-    config.report_types = [:html]
-    config.report_title = 'QA Engineer Challenge!'
-    config.additional_info = @infos
-    config.color = 'pink'
-  end
-  ReportBuilder.build_report
-end
+#   ReportBuilder.configure do |config|
+#     config.input_path = 'log/report.json'
+#     config.report_path = 'log/report'
+#     config.report_types = [:html]
+#     config.report_title = 'QA Engineer Challenge!'
+#     config.additional_info = @infos
+#     config.color = 'pink'
+#   end
+#   ReportBuilder.build_report
+# end
 
-After do |scenario|
-  if scenario.failed?
-    screenshot 'image.png'
-    embed('image.png', 'image/png')
-  end
-end
+# After do |scenario|
+#   if scenario.failed?
+#     screenshot 'image.png'
+#     embed('image.png', 'image/png')
+#   end
+# end
